2e4af6d1414b8f508606cf2ba89a13602edfdc5f
otumian <popecan1000@gmail.com>
2024-08-08 11:59:02 +0000

    add a common module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { AppConstant } from './constants/app.constant';
+<<<<<<< HEAD
 import { IdValidation } from './validation';
 
 @Global()
 @Module({
   providers: [AppConstant, IdValidation],
   exports: [AppConstant, IdValidation]
+=======
+
+@Global()
+@Module({
+  providers: [AppConstant],
+  exports: [AppConstant]
+>>>>>>> 9deef2e (add a common module)
 })
 export class CommonModule {}
